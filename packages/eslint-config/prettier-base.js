34bb2ae563f98d4f5666fc8c69bc090d5f8e3587
gong-yeongbin <youngbin816@gmail.com>
2025-07-14 13:38:50 +0900

    prettier, eslint setup

---
+++ packages/eslint-config/prettier-base.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export const prettierConfig = {
+  printWidth: 180,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+};
