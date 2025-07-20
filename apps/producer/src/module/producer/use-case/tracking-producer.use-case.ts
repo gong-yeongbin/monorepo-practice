b5fac28f04677aced046dded5c4964f7fd928ec0
gong-yeongbin <youngbin816@gmail.com>
2025-07-21 08:58:54 +0900

    dotenvx setup

--- apps/producer/src/module/producer/use-case/tracking-producer.use-case.ts
+++ apps/producer/src/module/producer/use-case/tracking-producer.use-case.ts
@@ -10,8 +10,11 @@ export class TrackingProducerUseCase {
 		// 캐시 조회 (token)
 		//-- 데이터베이스 캠페인 조회 (token)
 		// 리다이렉트 url
+
+		// ===== kafka producer =====
 		// 데일리 클릭 카운트 (token + pub + sub = view_code)
-		// 트래킹 로그
+		// 매체 전송
+		// 트래킹 로그?
 		return '';
 	}
 }
