229c0cb55e075221f06f97063c466b595ba21058
gong-yeongbin <youngbin816@gmail.com>
2025-07-24 08:00:31 +0900

    [tracker]: request data string trim

--- apps/consumer/src/module/tracker/dto/request/create-tracker.dto.ts
+++ apps/consumer/src/module/tracker/dto/request/create-tracker.dto.ts
@@ -1,15 +1,20 @@
 import { IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateTrackerDto {
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	name: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	tracking_url: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	install_postback_url: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	event_postback_url: string;
 }
--- apps/consumer/src/module/tracker/dto/request/update-tracker.dto.ts
+++ apps/consumer/src/module/tracker/dto/request/update-tracker.dto.ts
@@ -1,15 +1,20 @@
 import { IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UpdateTrackerDto {
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	name: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	trackingUrl: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	installPostbackUrl: string;
 
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	eventPostbackUrl: string;
 }
