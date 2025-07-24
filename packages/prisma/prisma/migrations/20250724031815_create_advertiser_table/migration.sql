53ebc25e174220c278c5779238000f821fd0e157
gong-yeongbin <youngbin816@gmail.com>
2025-07-24 12:40:10 +0900

    [prisma]: create advertiser table

---
+++ packages/prisma/prisma/migrations/20250724031815_create_advertiser_table/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `name` on the `media` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `VarChar(50)`.
+  - You are about to alter the column `name` on the `tracker` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `VarChar(50)`.
+
+*/
+-- AlterTable
+ALTER TABLE `media` MODIFY `name` VARCHAR(50) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `tracker` MODIFY `name` VARCHAR(50) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `advertiser` (
+    `name` VARCHAR(50) NOT NULL,
+
+    UNIQUE INDEX `advertiser_name_key`(`name`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
