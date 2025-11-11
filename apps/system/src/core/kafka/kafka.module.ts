c88cdec6576c31456ceb374d7a4ff38b66f4f000
gong-yeongbin <youngbin816@gmail.com>
2025-11-11 12:45:36 +0900

    [core]: kafka delete

--- apps/system/src/core/kafka/kafka.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KafkaProducerService } from '@core/kafka/kafka-producer.service';
-import { KafkaConsumerService } from '@core/kafka/kafka-consumer.service';
-
-@Module({
-	providers: [KafkaProducerService, KafkaConsumerService],
-	exports: [KafkaProducerService, KafkaConsumerService],
-})
-export class KafkaModule {}
