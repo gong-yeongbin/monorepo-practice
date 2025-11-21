5aec49ba9ce23a72dcebb0405380e7e5b445bc6e
gong-yeongbin <youngbin816@gmail.com>
2025-11-21 09:48:27 +0900

    [adminstrator-page]: login view test

--- apps/adminstrator-page/src/App.vue
+++ apps/adminstrator-page/src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <nav>
+      <router-link to="/login" />
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ apps/adminstrator-page/src/views/LoginView1.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const user_id = ref('')
+const user_pw = ref('')
+
+function onLogin() {
+  if (!user_id.value) return alert('ID를 입력하세요.')
+  if (!user_pw.value) return alert('PW를 입력하세요.')
+  alert('로그인 시도됨')
+}
+</script>
+
+<template>
+  <div class="login-page">
+    <form @submit.prevent="onLogin">
+      <input type="text" v-model="user_id" placeholder="아이디" />
+      <input type="text" v-model="user_pw" placeholder="비밀번호" />
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.login-page {
+  width: 350px;
+  margin: 0px auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
